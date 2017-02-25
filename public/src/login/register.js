c1be0c0f505a6098c03b4b2b05c3ef68ea9d5949
ugeHidalgo <ugeHidalgo@hotmail.com>
2017-02-25 10:05:34 +0100

    Added validation for password in both register and update user profile data

--- public/src/login/register.js
+++ public/src/login/register.js
@@ -1,20 +1,53 @@
 (function (angular) {
-    var theModule = angular.module ('register', ['ui.bootstrap']); 
+    var theModule = angular.
+                    module ('register', ['ui.bootstrap', 'ui-notification']).
+                    config (function (NotificationProvider) {
+                                NotificationProvider.setOptions ({
+                                    delay: 3000,
+                                    startTop: 20,
+                                    startRight: 10,
+                                    verticalSpacing: 20,
+                                    horizontalSpacing: 20,
+                                    positionX: 'center',
+                                    positionY: 'top'
+                                });
+                    }); 
     
     theModule.controller ('registerController',  [
-         '$scope', '$window', '$http',
-         function ($scope) {
+         '$scope', '$window', '$http', 'Notification',
+         function ($scope, $window, $http, Notification) {
+            var urlParts = $window.location.pathname.split('/'),
+                userName = urlParts[urlParts.length-1],
+                userProfileUrl = '/api/user/'+userName;
+
             $scope.email = '';
             $scope.username= '';
             $scope.errorMessage;
+
             $scope.changedPassword = function() {
-                if ($scope.confirmPassword !== $scope.password){
+                var pass = $scope.password,
+                    confirm = $scope.confirmPassword;
+
+                if ( pass.length != confirm.length || pass !== confirm ) {
                     $scope.errorMessage = 'Password and confirm are not equal.';
                 } else {
                     $scope.errorMessage = '';
                 }
             };
 
+            $scope.saveUser = function() {
+                if ( $scope.password !== $scope.confirmPassword ) {
+                    $scope.errorMessage = 'Password and confirm are not equal.';
+                    Notification.error('User was not updated: Password and confirm are not equal. ');
+                    return;
+                }
+                $http.post( userProfileUrl, $scope.user ).
+                    then(function (result) {
+                        Notification.success('User successfully updated.');
+                    }, function (error) {
+                        Notification.error('User was not updated: ' + error.statusText);
+                    }); 
+            };
         }
     ]);
 })(window.angular);
--- public/src/login/userProfile.js
+++ public/src/login/userProfile.js
@@ -1,30 +1,46 @@
 (function (angular) {
     var theModule = angular.
-        module ('userProfile', ['ui.bootstrap', 'ui-notification']).
-        config (function (NotificationProvider) {
-            NotificationProvider.setOptions ({
-                delay: 3000,
-                startTop: 20,
-                startRight: 10,
-                verticalSpacing: 20,
-                horizontalSpacing: 20,
-                positionX: 'center',
-                positionY: 'top'
-            });
-        });
+                    module ('userProfile', ['ui.bootstrap', 'ui-notification']).
+                    config (function (NotificationProvider) {
+                                NotificationProvider.setOptions ({
+                                    delay: 3000,
+                                    startTop: 20,
+                                    startRight: 10,
+                                    verticalSpacing: 20,
+                                    horizontalSpacing: 20,
+                                    positionX: 'center',
+                                    positionY: 'top'
+                                });
+                    });
     
     theModule.controller ('userProfileController',  [
          '$scope', '$window', '$http', 'Notification',
          function ($scope, $window, $http, Notification) {
+             
             var urlParts = $window.location.pathname.split('/'),
                 userName = urlParts[urlParts.length-1],
                 userProfileUrl = '/api/user/'+userName;
 
             $scope.user = {};
 
-            $scope.saveUser = function() {
+            $scope.changedPassword = function() {
+                var pass = $scope.user.password,
+                    confirm = $scope.user.confirmPassword;
 
-                $http.post( userProfileUrl, $scope.user).
+                if ( pass.length != confirm.length || pass !== confirm ) {
+                    $scope.errorMessage = 'Password and confirm are not equal.';
+                } else {
+                    $scope.errorMessage = '';
+                }
+            };
+
+            $scope.saveUser = function() {
+                if ( $scope.user.password !== $scope.user.confirmPassword ) {
+                    $scope.errorMessage = 'Password and confirm are not equal.';
+                    Notification.error('User was not updated: Password and confirm are not equal. ');
+                    return;
+                }
+                $http.post( userProfileUrl, $scope.user ).
                     then(function (result) {
                         Notification.success('User successfully updated.');
                     }, function (error) {
