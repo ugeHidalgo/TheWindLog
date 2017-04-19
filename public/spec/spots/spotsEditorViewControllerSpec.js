2554608f302c258287713089bc59255a41a27646
Eugenio Hidalgo Hernàndez <eugenio.hidalgo@unit4.com>
2017-04-19 17:06:07 +0200

    Added post tests for spotsEditor

--- public/spec/spots/spotsEditorViewControllerSpec.js
+++ public/spec/spots/spotsEditorViewControllerSpec.js
@@ -1,6 +1,7 @@
 describe ('spotsEditorView', function() {
 
-    var spotData = { "id": 1000, "Name": "spot1", "userName": "anyUserName"};
+    var spotData = { "id": 1000, "name": "spot1", "username": "anyUserName"},
+        savedSpotData = { "id": 1000, "name": "spot1-saved", "username": "anyUserName"};
 
     
     beforeEach(function() {
@@ -40,7 +41,7 @@ describe ('spotsEditorView', function() {
             });
         });
 
-        describe('when updating an existing spot', function() {
+        describe('when loading an existing spot', function() {
             var httpMock;
 
             beforeEach(inject(function(_$controller_, $httpBackend) {
@@ -52,13 +53,9 @@ describe ('spotsEditorView', function() {
                 httpMock = $httpBackend;
                 
                 ctrl = $controller('spotsEditorViewController', {$scope: $scope, $routeParams: $routeParamsMock, Notification, NgMap});
-            }));
+            }));            
 
-            it('should create the controller', function() {        
-                expect(ctrl).toBeDefined();
-            });
-
-            describe('with a success get call', function() {
+            describe('with a success GET call', function() {
 
                 beforeEach(function(){
                     spyOn( $scope, 'initMap' );
@@ -68,6 +65,81 @@ describe ('spotsEditorView', function() {
                 it('should load the spot to be updated', function() {        
                     httpMock.flush();      
                     expect($scope.spot).toEqual(spotData);
+                });
+            });
+
+            describe('with a failure GET call', function() {
+
+                var NotificationMock;
+
+                beforeEach(function(){
+                    notificationMock = Notification;
+                    notificationMock.error = function () {};
+                    spyOn(notificationMock,'error');
+                    spyOn( $scope, 'initMap' );
+                    httpMock.expectGET('/api/spots/anyUserName/1000').respond(500,'Error getting spot.');                                        
+                });
+
+                it('should get an error using Notification', function() {        
+                    httpMock.flush();      
+                    expect(Notification.error).toHaveBeenCalled();
+                });
+            });
+        });
+
+        describe('when saving a spot', function() {
+            var httpMock;
+
+            beforeEach(inject(function(_$controller_, $httpBackend) {
+                $controller = _$controller_;                
+                notificationMock = Notification;
+                notificationMock.success = function () {};
+                spyOn(notificationMock,'success');
+                $routeParamsMock = { userName: 'anyUserName', spotId: '1000'};
+                NgMap = { };
+
+                httpMock = $httpBackend;
+                
+                ctrl = $controller('spotsEditorViewController', {$scope: $scope, $routeParams: $routeParamsMock, Notification, NgMap});
+            }));
+
+            describe('with a success POST call', function() {
+
+                beforeEach(function(){
+                    spotData.updated = undefined;
+                    httpMock.expectPOST('/api/spots', savedSpotData).respond(200,savedSpotData);
+                });
+
+                it('should save the spot', function() {
+                    $scope.spot.name = 'spot1-saved';    
+                    $scope.saveItem();
+                    $scope.spot.updated = undefined; //This is to avoid problems with milisecs.
+                    httpMock.flush();                         
+                    expect($scope.spot.name).toEqual('spot1-saved');
+                });
+
+                it('should activate the busy indicator before save', function() {
+                    $scope.spot.name = 'spot1-saved';    
+                    $scope.saveItem();
+                    $scope.spot.updated = undefined; //This is to avoid problems with milisecs.
+                    expect($scope.busyIndicator).toBeTruthy();
+                    httpMock.flush();                                             
+                });
+
+                it('should get a Notification success message', function() {
+                    $scope.spot.name = 'spot1-saved';    
+                    $scope.saveItem();
+                    $scope.spot.updated = undefined; //This is to avoid problems with milisecs.
+                    httpMock.flush();                         
+                    expect(Notification.success).toHaveBeenCalled();
+                });
+
+                it('should deactivate the busy indicator after save', function() {
+                    $scope.spot.name = 'spot1-saved';    
+                    $scope.saveItem();
+                    $scope.spot.updated = undefined; //This is to avoid problems with milisecs.                    
+                    httpMock.flush();
+                    expect($scope.busyIndicator).toBeFalsy();
                 });
             });
         });
