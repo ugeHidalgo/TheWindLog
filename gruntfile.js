97675fdd4379250c227d0b172e835233e12cdd08
ugeHidalgo <ugeHidalgo@hotmail.com>
2017-05-06 16:17:31 +0200

    Use of sass and grunt to compile sass to css

--- gruntfile.js
+++ gruntfile.js
@@ -1,5 +1,37 @@
 module.exports = function (grunt){
     grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        sass: {
+            dev: {
+                options: {
+                    style: 'expanded',
+                },
+                files: {
+                    'public/css/site.css' : 'public/scss/site.scss',
+                    'public/css/creative.css' : 'public/scss/creative.scss',
+                    'public/css/switchOnOff.css' : 'public/scss/switchOnOff.scss'
+                }
+            },
+            dist: {
+                options: {
+                style: 'compressed',
+                },
+                files: {
+                    'public/css/site.min.css' : 'public/scss/site.scss',
+                    'public/css/creative.min.css' : 'public/scss/creative.scss',
+                    'public/css/switchOnOff.min.css' : 'public/scss/switchOnOff.scss'
+                }
+            }
+        },
+
+        watch: {
+            css: {
+                files: '**/*.scss',
+                tasks: ['sass']
+            }
+        },
+
         nodemon: {
             all: {
                 script: 'server.js',
@@ -10,6 +42,8 @@ module.exports = function (grunt){
         }
     });
 
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-nodemon');
-    grunt.registerTask('default', ['nodemon']);
+    grunt.registerTask('default', ['nodemon','watch']);
 }
