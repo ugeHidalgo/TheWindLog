3d0b86f0d37001c879a02d19e7bf16b5c9f834eb
ugeHidalgo <ugeHidalgo@hotmail.com>
2017-05-12 20:29:13 +0200

    Added axis to sessions count chart

--- public/src/spots/spotsStatsViewController.js
+++ public/src/spots/spotsStatsViewController.js
@@ -15,7 +15,6 @@ angular
 
             var urlParts = $window.location.hash.split('/'),
                 userName = urlParts[2],
-                sessions = [],
                 url = '/api/sessionstotals/' + userName;
 
             $scope.userName = userName;
@@ -29,14 +28,8 @@ angular
                 .then(function (result) {
                     //Success
                     $scope.sessionsTotals = result.data;
-                    drawSessionsCountChart($scope.sessionsTotals);
-                    /*$scope.sessionsTotals.forEach(function(sessionData) {
-                        sessions.push (sessionData.sessionsCount);
-                        if (sessions.length === $scope.sessionsTotals.length &&
-                            sessions.length >0) {
-                                drawSessionsCountChart($scope.sessionsTotals);
-                        }
-                    });*/
+                    drawSessionsCountChart($scope.sessionsTotals, "#ch1.chart");
+                    drawSessionsCountChart($scope.sessionsTotals, "#ch2.chart");
                 }, function (error) {
                     //Error
                     Notification.error ('Failed to get sessions totals !!');
@@ -45,40 +38,57 @@ angular
                     $scope.busyIndicator = false;
                 });
 
-            function drawSessionsCountChart(data) {
+            function drawSessionsCountChart(data, chartId) {
 
-            
-                var margin = { top: 20, right: 30, bottom: 20, left: 30},
-                    chartWidth = 200,
-                    chartHeight = 325,
-                    chart, bar,
-                    scaleY = d3.scale.linear() //Escala el width de la barra al alto del div    
-                                .range([chartHeight, 0]);
-                                //alto del div
+                var margin = { top: 20, right: 30, bottom: 30, left: 40},
+                    chartWidth = 500 - margin.left - margin.right,
+                    chartHeight = 325 - margin.top - margin.bottom,
+                    chart, chartbar,
+                    barWidth = chartWidth / data.length; //El ancho de la barra
+
+                var x = d3.scale.ordinal()
+                                .rangeRoundBands([0,chartWidth], .1)
+                                .domain(data.map( function(d) { return d._id[0].name; })),
+                    y = d3.scale.linear() //Escala el width de la barra al alto del div    
+                                .range([chartHeight, 0])
+                                .domain([0, d3.max(data, function(d) { return d.sessionsCount; })]);
+                
+                var xAxis = d3.svg.axis()
+                                .scale(x)
+                                .orient("bottom"),
+                    yAxis = d3.svg.axis()
+                                .scale(y)
+                                .orient("left")
+                                .ticks(d3.max(data, function(d) { return d.sessionsCount; }), "%");
+
+                var chart = d3.select(chartId)
+                            .attr("width", chartWidth + margin.left + margin.right )
+                            .attr("height", chartHeight + margin.bottom + margin.top )
+                        .append("g") //Para centrar el chart con los margenes definidos.
+                            .attr("transform","translate(" + margin.left + "," + margin.top + ")");
+
+                chart.append("g")
+                            .attr("class", "x axis")
+                            .attr("transform", "translate (0," + chartHeight + ")")
+                            .call(xAxis);
+                chart.append("g")
+                        .attr("class", "y axis")
+                        .call(yAxis)
+                    .append("text")
+                        .attr("transform", "rotate(-90)")
+                        .attr("y",6)
+                        .attr("dy", ".71em")
+                        .style("text-anchor","end")
+                        .text("Sessions count");
                                 
-                scaleY.domain([0, d3.max(data, function(d) { return d.sessionsCount; })]);
-
-                var barWidth = chartWidth / data.length; //El ancho de la barra
-
-                var chart = d3.select("#ch1.chart")
-                                .attr("width", chartWidth )
-                                .attr("height", chartHeight);
-                                
-                var bar = chart.selectAll("div")
-                                .data(data)
-                                .enter().append("g")
-                                .attr("transform", function(d, i) { return "translate(" + i * barWidth + ",0)"; }); //Desplazamiento de la barra hacia la derecha
-
-                bar.append("rect")
-                    .attr("y", function(d) { return scaleY(d.sessionsCount); })
-                    .attr("height", function(d) { return chartHeight - scaleY(d.sessionsCount); }) //Para darle la vuelta a la barra ya que el 0,0 es el top left.
-                    .attr("width", barWidth - 1);
-
-                bar.append("text")
-                    .attr("x", barWidth / 2 ) //x left 
-                    .attr("y", function(d) { return scaleY(d.sessionsCount) + 3; }) //y bottom
-                    .attr("dy", ".75em")
-                    .text(function(d) { return d.sessionsCount; });
+                chart.selectAll(".bar")
+                        .data(data)
+                    .enter().append("rect")
+                        .attr("class","bar")
+                        .attr("x", function(d) { return x(d._id[0].name); })
+                        .attr("y", function(d) { return y(d.sessionsCount); })
+                        .attr("height", function(d) { return chartHeight - y(d.sessionsCount); }) //Para darle la vuelta a la barra ya que el 0,0 es el top left.
+                        .attr("width", x.rangeBand());
             };
 
             function type(d) {
