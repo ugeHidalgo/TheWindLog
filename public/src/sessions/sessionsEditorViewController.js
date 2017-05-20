afb292beef27a48d178afc1b58f7c14f313193ac
ugeHidalgo <ugeHidalgo@hotmail.com>
2017-05-20 21:28:14 +0200

    New helper class to hold time conversions

--- public/src/sessions/sessionsEditorViewController.js
+++ public/src/sessions/sessionsEditorViewController.js
@@ -190,26 +190,5 @@ angular
                     marker.setMap(map);
                 });
             };
-
-            function secondsToTime (secondsAmount) {
-                var days  = 1,
-                    hours = Math.floor (secondsAmount / 3600),
-                    minutes = Math.floor ((secondsAmount % 3600) / 60),
-                    seconds = Math.floor ((secondsAmount % 3600) % 60);
-
-                return new Date(1970, 0, days, hours, minutes, seconds);
-            };
-
-            function timeToSeconds (timeString) {
-                var hours = 0,
-                    minutes = 0,
-                    seconds = 0;
-                if (timeString) {
-                    hours = timeString.getHours();
-                    minutes = timeString.getMinutes();
-                    seconds = timeString.getSeconds();
-                }
-                return hours + 3600 + minutes * 60 + seconds;
-            };
         }
 ]);
