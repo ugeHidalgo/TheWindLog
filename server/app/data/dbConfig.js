3a70652927ba6cf36304a01d72cd3284401cee77
Eugenio Hidalgo Hernàndez <eugenio.hidalgo@unit4.com>
2017-05-22 16:22:08 +0200

    DB migrated to mLab instead of local.

--- server/app/data/dbConfig.js
+++ server/app/data/dbConfig.js
@@ -1,3 +1,4 @@
 module.exports = {
-    'url' : 'mongodb://localhost:27017/windlogDB'
+    //'url' : 'mongodb://localhost:27017/windlogDB' //DB on local computer
+    'url' : 'mongodb://ugeHidalgo:gunnar12A@ds149501.mlab.com:49501/windlogdb' //DB on mLab
 }
